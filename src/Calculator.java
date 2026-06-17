c8a59afd80b2348fd055895909e0fe5826e3a134
Lucas_hgs <lucas.henrique.gois.santos@gmail.com>
2026-06-16 22:05:40 -0300

    Adding the display and buttons

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,14 +1,29 @@
 import java.awt.*;
-import java.awt.event.*;
-import java.util.Arrays;
 import javax.swing.*;
-import javax.swing.border.LineBorder;
 
 public class Calculator {
     int window_width = 360;
     int window_height = 540;
 
+    Color customBlack = new Color(0x071721);
+    Color customDarkBlue = new Color(0x30455c);
+    Color customPaleOrange = new Color(0xc07e53);
+    Color customEggshellWhite =new Color(0xf8eacf);
+
+    String[] buttonValues = {
+            "AC", "+/-", "%", "÷",
+            "7", "8", "9", "×",
+            "4", "5", "6", "-",
+            "1", "2", "3", "+",
+            "0", ",", "√", "="
+    };
+    String[] rightSymbols = {"÷", "×", "-", "+", "="};
+    String[] topSymbols = {"AC", "+/-", "%"};
+
     JFrame window = new JFrame("Java Calculator");
+    JLabel displayLabel = new JLabel();
+    JPanel displayPanel = new JPanel();
+    JPanel buttonsPanel = new JPanel();
 
     Calculator() {
         window.setVisible(true);
@@ -17,5 +32,28 @@ public class Calculator {
         window.setResizable(false);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setLayout(new BorderLayout());
+
+        displayLabel.setBackground(customBlack);
+        displayLabel.setForeground(customEggshellWhite);
+        displayLabel.setFont(new Font("Ariel", Font.PLAIN, 80));
+        displayLabel.setHorizontalAlignment(JLabel.RIGHT);
+        displayLabel.setText("0");
+        displayLabel.setOpaque(true);
+
+        displayPanel.setLayout(new BorderLayout());
+        displayPanel.add(displayLabel);
+        window.add(displayPanel, BorderLayout.NORTH);
+
+        buttonsPanel.setLayout(new GridLayout(5,4));
+        buttonsPanel.setBackground(customBlack);
+        window.add(buttonsPanel);
+
+        for (int i=0; i<buttonValues.length; i++) {
+            JButton button = new JButton();
+            String buttonValue = buttonValues[i];
+            button.setFont(new Font("Ariel", Font.PLAIN, 30));
+            button.setText(buttonValue);
+            buttonsPanel.add(button);
+        }
     }
 }
